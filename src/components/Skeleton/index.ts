6c085449dec6b895d25a18044b0ff97a4fde9a58
nguyenpro22 <dungcvse160200@fpt.edu.vn>
2024-10-07 22:10:33 +0700

    add loading animation for orders

--- src/components/Skeleton/index.ts
+++ src/components/Skeleton/index.ts
@@ -2,3 +2,4 @@ export { default as CategorySkeleton } from "./Category";
 export { default as ProductDetailSkeleton } from "./ProductDetail";
 export { default as CardSkeleton } from "./CardProduct";
 export { default as ImageSliderSkeleton } from "./ImageSlider";
+export { TableRowSkeleton } from "./Table";
