484a748a561e9601fd4ac53520f12d11c69b8e3f
nguyenpro22 <dungcvse160200@fpt.edu.vn>
2024-10-07 21:30:32 +0700

    config notfound page

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+
+  if (url.pathname.startsWith("/admin")) {
+    const token = req.cookies.get("jwt")?.value;
+
+    if (!token) {
+      url.pathname = "/404";
+      return NextResponse.rewrite(url);
+    }
+  }
+
+  return NextResponse.next();
+}
